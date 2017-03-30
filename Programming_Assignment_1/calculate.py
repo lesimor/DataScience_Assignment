ae37160a90cd092e7e083a0ba2ff6070ad4abe13
강병욱 <2011021764@hanyang.ac.kr>
2017-03-30 20:11:49 +0900

    주석달기 완료

--- Programming_Assignment_1/calculate.py
+++ Programming_Assignment_1/calculate.py
@@ -3,72 +3,92 @@ import calculate
 import itertools
 
 
+# get statistic information of set in file
 def set_stat(_file, _set):
-    _file.seek(0)  # 커서를 초기화
-    frequency = 0  # 빈도수 변수 초기화
-    transaction_count = 0
-    result_obj = {}
+    _file.seek(0)  # initialize cursor
+    frequency = 0  # initialize frequency variable
+    transaction_count = 0   # count all transaction size
+    result_obj = {}         # variable for result value
+
+    # read by line
     for line in _file:
         transaction_count += 1
-        transaction = map(int, line.split())
-        transaction_set = set(transaction)
-        if _set.issubset(transaction_set):
+        transaction = map(int, line.split())    # convert transaction string into integer list
+        transaction_set = set(transaction)      # convert list into set
+        if _set.issubset(transaction_set):      # add frequency if parameter set belongs to transaction set
             frequency += 1
     result_obj["frequency"] = frequency
     result_obj["transaction_count"] = transaction_count
-    result_obj["support"] = float(frequency) / float(transaction_count)
+    result_obj["support"] = (float(frequency) / float(transaction_count)) * 100
     return result_obj
 
 
+# passing item_set value and associative_item_set value
+# calculate support and confidence
 def associative_set_stat(_file, item_set, associative_item_set):
-    # item_set 발생 빈도수
+    # item_set frequency
     item_set_frequency = set_stat(_file, item_set)["frequency"]
 
+    # generate union set of item set and associative item set
     union_set = item_set.union(associative_item_set)
-    # item_set U associative_item_set 빈도수
+
+    # get frequency of union set
     confidence_frequency = set_stat(_file, union_set)["frequency"]
 
     return {
         'support': set_stat(_file, union_set)["support"],
-        'confidence': float(confidence_frequency) / float(item_set_frequency)
+        'confidence': (float(confidence_frequency) / float(item_set_frequency)) * 100
     }
 
 
+# join and prune method
 def join_and_prune(_file, set_list, min_sup, step):
-    result_set_list = []
+    result_set_list = []    # variable for return value
     for index, s in enumerate(set_list):
         for x in range(index + 1, len(set_list)):
             union_set = s.union(set_list[x])
 
+            # add on result only if length of union set is equal to step and support of union set is higher than min_sup
             if len(union_set) == step and calculate.set_stat(_file,
                                                              union_set)[
-                "frequency"] >= min_sup and union_set not in result_set_list:
-                print str(union_set) + str(calculate.set_stat(_file, union_set)["frequency"])
+                "support"] >= min_sup and union_set not in result_set_list:
                 result_set_list.append(union_set)
+    # return result set list if the list's size is more than zero
     if len(result_set_list) > 0:
         return result_set_list
+    # return None if result set list's size is zero
     else:
         return None
 
 
+# generate item set and associative set among parameter set
 def generate_item_associative(_set):
     set_size = len(_set)
     combination_list = []
     for size in range(1, set_size):
+        # generate combination of set by size
         items = map(set, itertools.combinations(list(_set), size))
+
         for item in items:
             item_set = item
             associative_set = _set - item_set
+
+            # append item set and associative set pair on combination list
             combination_list.append([item_set, associative_set])
 
     return combination_list
 
 
+# generate all subset of parameter set
 def generate_all_subsets(_set):
+    # get length of parameter set
     set_size = len(_set)
+
+    # variable for return value
     subset_list = []
+
+    # generate subset of each total size
     for size in range(1, set_size + 1):
         items = map(set, itertools.combinations(list(_set), size))
-        subset_list += items
+        subset_list += items    # append item on subset list
     return subset_list
-
--- Programming_Assignment_1/program.py
+++ Programming_Assignment_1/program.py
@@ -2,60 +2,78 @@
 # 우선 값을 읽어서 빈도수를 체크
 # 파일을 읽기위해 연다.
 import calculate
+import sys
 
-f = open('./input.txt')
-f.readline()
+# minimal_support(%)
+minimal_support_argv = sys.argv[1]
 
-# set의 집합
+# input file path
+input_file = sys.argv[2]
+
+# output file path
+output_file = sys.argv[3]
+
+# type casting to float
+minimal_support = float(minimal_support_argv)
+
+# file open and read
+f = open(input_file)
+
+
+# table of set
 frequency_table = []
 
-# 입력받은 support
-minimal_support = 50
-
+# read by line
 for line in f:
-    # 모든 경우의 수를 산출
+    # convert each transaction into list
     transaction = map(int, line.split())
+
+    # generate all occurence by list
     for num in transaction:
         if not set([num]) in frequency_table:
+            # insert unique value into table of set
             frequency_table.append(set([num]))
 
-# minimal support에 미치지 못하는 값은 삭제
+# prune by minimal support
+print "1 step proceeding..."
+result = [s for s in frequency_table if calculate.set_stat(f, s)["support"] >= minimal_support]
 
-l_1 = [s for s in frequency_table if calculate.set_stat(f, s)["frequency"] >= minimal_support]
-result = l_1
+# temporary saving space
 result_save = []
+
+# join_and_pruning step
 step = 2
+
+# join and prune until further progress is no longer proceeding.
 while calculate.join_and_prune(f, result, minimal_support, step):
     result = calculate.join_and_prune(f, result, minimal_support, step)
     if not result:
         break
-    print step
+    print str(step)+" step proceeding..."
     step += 1
     result_save = result
 
-print result_save
+# open file for writing
+output_file = open(output_file, 'w')
 
-# 검증
-output_file = open("output.txt", 'w')
+# result set
 for s in result_save:
+    # generate all subsets
     subsets = calculate.generate_all_subsets(s)
+
+    # generate combination of subsets and calculate support and confidence of them
     for subset in subsets:
         combinations = calculate.generate_item_associative(subset)
+
+        # file write after each combinations
         for item_associative in combinations:
             r = calculate.associative_set_stat(f, item_associative[0], item_associative[1])
-            print_line = '{%s}\t{%s}\t%.1f\t%.1f\n' % (",".join(map(str, list(item_associative[0]))),
-                                                       ",".join(map(str, list(item_associative[1]))),
-                                                       r["support"] * 100,
-                                                       r["confidence"] * 100)
+            print_line = '%10s\t%10s\t%.1f\t%.1f\n' % ("{"+",".join(map(str, list(item_associative[0]))) + "}",
+                                                       "{"+",".join(map(str, list(item_associative[1]))) + "}",
+                                                       r["support"],
+                                                       r["confidence"])
             output_file.write(print_line)
+print "done"
 
-print calculate.associative_set_stat(f, set([1, 2]), set([11]))
-
-print calculate.generate_item_associative(set([1, 2, 3, 4]))
-
-print calculate.generate_all_subsets(set([1, 2, 3, 4, 5]))
-
+# close file
 f.close()
-
-# L2 generate
-#
